218857db75102d4c66bae55f42629f168085e676
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-14 15:44:24 -0500

    added the topics array to the js and started adding in some divs to fill in index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,17 @@
+topics = [
+    "Jake Peralta",
+    "Rosa Diaz",
+    "Terry Jeffords",
+    "Amy Santiago",
+    "Charles Boyle",
+    "Gina Linetti",
+    "Raymond Jacob Holt",
+    "Michael Hitchcock",
+    "Norm Scully",
+    "the 99"           
+]
+
 $(document).ready(function(){
 
-    
+
 });
