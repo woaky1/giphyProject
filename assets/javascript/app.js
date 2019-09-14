29d22fdfb35e0e1ad4b53a956934b687768dd2d6
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-14 16:15:51 -0500

    Got the buttons based on the array to display.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,17 @@
-topics = [
-    "Jake Peralta",
-    "Rosa Diaz",
-    "Terry Jeffords",
-    "Amy Santiago",
-    "Charles Boyle",
-    "Gina Linetti",
-    "Raymond Jacob Holt",
-    "Michael Hitchcock",
-    "Norm Scully",
-    "the 99"           
-]
+topics = ["Jake Peralta", "Rosa Diaz", "Terry Jeffords", "Amy Santiago", "Charles Boyle", "Gina Linetti", "Raymond Jacob Holt", "Michael Hitchcock", "Norm Scully", "the 99"];
 
 $(document).ready(function(){
 
+    function buttonMaker() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $("<button>");
+            a.addClass("99-btn");
+            a.attr("data-name", topics[i]);
+            a.text(topics[i]);
+            $("#buttons-view").append(a);
+        }
+    }
 
+    buttonMaker();
 });
