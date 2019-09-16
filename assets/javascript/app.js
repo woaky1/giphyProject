b9aa744d1ee3c575af3c888f9f1f8acab1dcb84b
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-15 20:37:02 -0500

    got the call to giphy working and still images to display. It's ugly, but it's working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-topics = ["Jake Peralta", "Rosa Diaz", "Terry Jeffords", "Amy Santiago", "Charles Boyle", "Gina Linetti", "Raymond Jacob Holt", "Michael Hitchcock", "Norm Scully", "the 99"];
+topics = ["Jake Peralta", "Rosa Diaz", "Terry Jeffords", "Amy Santiago", "Charles Boyle", "Gina Linetti", "Raymond Holt", "Michael Hitchcock", "Norm Scully", "the 99"];
 
 $(document).ready(function(){
 
@@ -19,5 +19,27 @@ $(document).ready(function(){
         topics.push(newTopic);
         buttonMaker();
     });
+
+    function displayGifs() {
+        var topicToDisplay = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uHRSYqUJ9J8SbR9afkR7ER1ahpRP40py&q=" + topicToDisplay + "&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            for (var i = 0; i < 10; i++) {
+                var gifDiv = $("<div class='gif'>");
+                var gifURL = response.data[i].images.fixed_height_still.url;
+                var gifImage = $("<img>").attr("src",gifURL);
+                gifDiv.append(gifImage);
+                $("#gifLand").prepend(gifDiv);
+            }
+        });
+    };
+
+    $(document).on("click", ".99-btn", displayGifs);
+
     buttonMaker();
 });
