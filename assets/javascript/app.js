bf80aa0d760c990149d1c1d1c99762f86b831163
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-16 16:13:37 -0500

    got an 'add more gifs' button working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 topics = ["Jake Peralta", "Rosa Diaz", "Terry Jeffords", "Amy Santiago", "Charles Boyle", "Gina Linetti", "Raymond Holt", "Hitchcock 99", "Scully 99", "the 99"];
+var offsetValue = 0;
+var topicToDisplay;
 
 $(document).ready(function(){
 
@@ -21,8 +23,50 @@ $(document).ready(function(){
     });
 
     function displayGifs() {
-        var topicToDisplay = $(this).attr("data-name");
+        topicToDisplay = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uHRSYqUJ9J8SbR9afkR7ER1ahpRP40py&q=" + topicToDisplay + "&limit=10&rating=pg";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            $("#addMore").removeAttr("class","hidden");
+            for (var i = 0; i < 10; i++) {
+                offsetValue = 0;
+                var gifDiv = $("<div class='gif float-left m-1'>");
+                var gifURL = response.data[i].images.fixed_height_still.url;
+                var gifStillURL = response.data[i].images.fixed_height_still.url;
+                var gifMovingURL = response.data[i].images.fixed_height.url;
+                var ratingValue = response.data[i].rating;
+                var ratingText = $("<p>Rating: " + ratingValue + "</p>");
+                var gifImage = $("<img>").attr({"src":gifURL,"data-still":gifStillURL,"data-move":gifMovingURL,"data-isMoving":"no","class":"isAGIF"});
+                gifDiv.append(ratingText);
+                gifDiv.append(gifImage);
+                $("#gifLand").prepend(gifDiv);
+            }
+        });
+    };
+
+
+    $(document).on("click", ".99-btn", displayGifs);
+
+
+    $(document).on("click",".isAGIF", function() {
+        // console.log(this);
+        var playingYet = $(this).attr("data-isMoving");
+        var stillURL = $(this).attr("data-still");
+        var movingURL = $(this).attr("data-move");
+        if(playingYet == "no") {
+            $(this).attr({"src":movingURL,"data-isMoving":"yes"});
+        } else {
+            $(this).attr({"src":stillURL,"data-isMoving":"no"});
+        }
+    });
+
+    function addMoreGifs() {
+        offsetValue += 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uHRSYqUJ9J8SbR9afkR7ER1ahpRP40py&q=" + topicToDisplay + "&limit=10&rating=pg&offset=" + offsetValue;
 
         $.ajax({
             url: queryURL,
@@ -42,22 +86,9 @@ $(document).ready(function(){
                 $("#gifLand").prepend(gifDiv);
             }
         });
-    };
+    }
 
-
-    $(document).on("click", ".99-btn", displayGifs);
-
-    $(document).on("click",".isAGIF", function() {
-        // console.log(this);
-        var playingYet = $(this).attr("data-isMoving");
-        var stillURL = $(this).attr("data-still");
-        var movingURL = $(this).attr("data-move");
-        if(playingYet == "no") {
-            $(this).attr({"src":movingURL,"data-isMoving":"yes"});
-        } else {
-            $(this).attr({"src":stillURL,"data-isMoving":"no"});
-        }
-    });
+    $("#addMore").on("click", addMoreGifs);
 
     buttonMaker();
 });
