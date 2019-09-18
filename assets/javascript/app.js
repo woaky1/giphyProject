73053dd808f81e61a48f51e34cd5a59a568a6414
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-17 21:48:03 -0500

    Added a whole bunch of documentation.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
+// Setting up some initial global variables and our initial set of topic buttons.
 topics = ["Jake Peralta", "Rosa Diaz", "Terry Jeffords", "Amy Santiago", "Charles Boyle", "Gina Linetti", "Raymond Holt", "Hitchcock 99", "Scully 99", "the 99"];
 var offsetValue = 0;
 var topicToDisplay;
 var favoriteArray = [];
 
 $(document).ready(function(){
-
+    // This function creates the topic buttons that appear at the top of the page.
     function buttonMaker() {
         $("#buttons-view").empty();
         for (var i = 0; i < topics.length; i++) {
@@ -15,7 +16,7 @@ $(document).ready(function(){
             $("#buttons-view").append(a);
         }
     }
-
+    // This is a event listener that takes into fed into the new topic form and then feeds that info into the buttonMaker function
     $("#add-topic").on("click", function(event) {
         event.preventDefault();
         var newTopic = $("#topic-input").val().trim();
@@ -23,6 +24,7 @@ $(document).ready(function(){
         buttonMaker();
     });
 
+    // This fuctiion takes data we got from the topic button and sends it to the API to get our gif information. We then, in turn, use that dats to generate new divs containing those gifs, favorite buttons associated with them, etc.
     function displayGifs() {
         topicToDisplay = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uHRSYqUJ9J8SbR9afkR7ER1ahpRP40py&q=" + topicToDisplay + "&limit=10&rating=pg";
@@ -50,10 +52,10 @@ $(document).ready(function(){
         });
     };
 
-
+    // This is an event listener checking to see if a user has clicked a topic button.
     $(document).on("click", ".99-btn", displayGifs);
 
-
+    // This is an event listener checking to see if the user has clicked a gif, and then toggling the moving image of the gif on and off.
     $(document).on("click",".isAGIF", function() {
         // console.log(this);
         var playingYet = $(this).attr("data-isMoving");
@@ -66,6 +68,7 @@ $(document).ready(function(){
         }
     });
 
+    // This function requests and displays more gifs. Very similar to displayGifs, except it incorportates offset into the API call so we don't get the same 10 gifs.
     function addMoreGifs() {
         offsetValue += 10;
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uHRSYqUJ9J8SbR9afkR7ER1ahpRP40py&q=" + topicToDisplay + "&limit=10&rating=pg&offset=" + offsetValue;
@@ -91,6 +94,7 @@ $(document).ready(function(){
         });
     }
 
+    // This function displays the the gifs marked as favorites in the favorite gif section. Similar to diplayGifs, except without the API call and with less data being set.
     function displayFavorites() {
         $("#favoriteLand").empty();
         for (var k = 0; k < favoriteArray.length; k++) {
@@ -104,6 +108,7 @@ $(document).ready(function(){
         }
     }
 
+    // This function lets us set a gif is a favorite (and remove it from favorites if it gets clicked again.)
     function makeFavorite() {
         if ($(this).attr("data-favorite") == "no") {
             $(this).attr({"data-favorite":"yes","class":"fav-button btn-danger"});
@@ -131,9 +136,12 @@ $(document).ready(function(){
         
     };
     
+    // This is an event listener to see if the user has clicked the "Add more gifs" button, and runs the addMoreGifs function if they do.
     $("#addMore").on("click", addMoreGifs);
 
+    // This is an event listener to see if the use has clicked a favorite button, and runs the makeFavorite if they do.
     $(document).on("click", ".fav-button", makeFavorite);
 
+    // Here we call buttonMaker so we can populate the initial set of buttons when the page first loads.
     buttonMaker();
 });
